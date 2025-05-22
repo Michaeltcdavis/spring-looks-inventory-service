2a958845d413cbe17cc4b883450d7d6b0bcf9d4d
Michael Davis <michaeltcdavis@gmail.com>
2025-05-22 14:18:01 -0400

    add integration test for inventory service

--- src/test/java/com/springlooks/inventory_service/InventoryServiceApplicationTests.java
+++ src/test/java/com/springlooks/inventory_service/InventoryServiceApplicationTests.java
@@ -1,15 +1,59 @@
 package com.springlooks.inventory_service;
 
+import io.restassured.RestAssured;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.testcontainers.containers.MySQLContainer;
+
+import java.lang.reflect.Type;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.4.5");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	public void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mysql.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldBeInStock() {
+		Boolean positive = RestAssured.given()
+				.get("/api/inventory?skuCode=white shoe&quantity=1")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+		Assertions.assertTrue(positive);
+
+		Boolean negative = RestAssured.given()
+				.get("/api/inventory?skuCode=white shoe&quantity=101")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+
+		Assertions.assertFalse(negative);
 	}
 
 }
